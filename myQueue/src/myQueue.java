f5c88473509d2dca65a3f9493785744cdde710eb
liujinxi931204 <liujx931204@163.com>
2021-01-13 19:26:10 +0800

    链式队列

--- myQueue/src/myQueue.java
+++ myQueue/src/myQueue.java
@@ -24,36 +24,82 @@ public class myQueue {
             this.queue=new int[capacity];
             this.n=capacity;
         }
-
         /**
          * 入队操作
+         * 均摊时间复杂度为O(1),因为只有在插入n次后才会整体数据搬移一次，时间复杂度为O(n)
+         * 均摊下来就是执行一次插入，有一次搬移，所以均摊时间复杂度为O(1)
+         *
          */
+
         public boolean enqueue(int data){
-            //队列满，入队失败
-            if(tail==n)
-                return false;
+            if(tail==n){
+                //队列满
+                if(head==0){
+                    return false;
+                }
+                //数据搬移
+                for (int i = head; i < tail; i++) {
+                    queue[i-head]=queue[i];
+                }
+
+                //使用系统方法也可
+//                if(tail-head>=0){
+//                    System.arraycopy(queue, head, queue, head - head, tail - head);
+//                }
+                tail=tail-head;
+                head=0;
+                //清空
+                for (int i = tail+1; i < n; i++) {
+                    queue[i]=0;
+                }
+            }
             queue[tail++]=data;
             return true;
         }
 
+
         /**
          * 出队操作
-         * @return
          */
+
         public int dequeue(){
-            //head==tail，表示队列为空
-            if(head==tail)
+            //队列空
+            if(head==tail){
                 return Integer.parseInt(null);
-            int data=queue[head];
-            for (int i = 1; i <tail; i++) {
-                queue[i-1]=queue[i];
             }
-            --tail;
-            return data;
+            return queue[head++];
+
         }
 
+        /**
+         * 入队操作
+         */
+//        public boolean enqueue(int data){
+//            //队列满，入队失败
+//            if(tail==n)
+//                return false;
+//            queue[tail++]=data;
+//            return true;
+//        }
+//
+//        /**
+//         * 出队操作
+//         * @return
+//         */
+//        public int dequeue(){
+//            //head==tail，表示队列为空
+//            if(head==tail)
+//                return Integer.parseInt(null);
+//            int data=queue[head];
+//            for (int i = 1; i <tail; i++) {
+//                queue[i-1]=queue[i];
+//            }
+//            --tail;
+//            return data;
+//        }
+
         public void list(){
-            for (int i = 0; i < tail; i++) {
+            for (int i = head; i < tail; i++) {
                 System.out.print(queue[i]+"  ");
             }
         }
@@ -113,49 +159,49 @@ public class myQueue {
     }
 
     public static void main(String[] args) {
-//        ArrayQueue arrayQueue = new ArrayQueue(3);
-//        arrayQueue.list();
-//        System.out.println();
-//        arrayQueue.enqueue(1);
-//        arrayQueue.enqueue(2);
-//        arrayQueue.enqueue(3);
-//        arrayQueue.list();
-//        System.out.println();
-//        int data1 = arrayQueue.dequeue();
-//        System.out.println(data1);
-//        arrayQueue.list();
-//        System.out.println();
-//        int data2 = arrayQueue.dequeue();
-//        System.out.println(data2);
-//        arrayQueue.list();
-//        System.out.println();
-//        int data3 = arrayQueue.dequeue();
-//        System.out.println(data3);
-//        arrayQueue.list();
-//        System.out.println();
-//        arrayQueue.list();
-
-
-        LinkedQueue linkedQueue = new LinkedQueue();
-        linkedQueue.list();
+        ArrayQueue arrayQueue = new ArrayQueue(3);
+        arrayQueue.list();
         System.out.println();
-        linkedQueue.enqueue(1);
-        linkedQueue.enqueue(2);
-        linkedQueue.enqueue(3);
-        linkedQueue.list();
+        arrayQueue.enqueue(1);
+        arrayQueue.enqueue(2);
+        arrayQueue.enqueue(3);
+        arrayQueue.list();
         System.out.println();
-        int data1 = linkedQueue.dequeue();
+        int data1 = arrayQueue.dequeue();
         System.out.println(data1);
-        linkedQueue.list();
+        arrayQueue.list();
         System.out.println();
-        int data2 = linkedQueue.dequeue();
+        int data2 = arrayQueue.dequeue();
         System.out.println(data2);
-        linkedQueue.list();
+        arrayQueue.list();
         System.out.println();
-        int data3 = linkedQueue.dequeue();
+        int data3 = arrayQueue.dequeue();
         System.out.println(data3);
-        linkedQueue.list();
+        arrayQueue.list();
         System.out.println();
+        arrayQueue.list();
+
+
+//        LinkedQueue linkedQueue = new LinkedQueue();
+//        linkedQueue.list();
+//        System.out.println();
+//        linkedQueue.enqueue(1);
+//        linkedQueue.enqueue(2);
+//        linkedQueue.enqueue(3);
+//        linkedQueue.list();
+//        System.out.println();
+//        int data1 = linkedQueue.dequeue();
+//        System.out.println(data1);
+//        linkedQueue.list();
+//        System.out.println();
+//        int data2 = linkedQueue.dequeue();
+//        System.out.println(data2);
+//        linkedQueue.list();
+//        System.out.println();
+//        int data3 = linkedQueue.dequeue();
+//        System.out.println(data3);
+//        linkedQueue.list();
+//        System.out.println();
 
     }
 }
