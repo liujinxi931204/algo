680edbcd54d71bec5774a24bb989c2caa6e524fe
liujinxi <liujx931204@163.com>
2021-11-02 16:29:57 +0800

    update

--- sort/src/FindNth.java
+++ sort/src/FindNth.java
@@ -1,51 +1,51 @@
 /**
  * 查询一个无序数组中第K大的数，
  * 思路就是使用快排中partition函数一样，以数组的最后一个数做pivot
- * 比pivot大的数都在左边，比pivot小的数都在右边，pivot的位置为p
- * 如果p+1=k,说明p位置上的数就是需要找的，
- * 如果k<p+1,说明要找的数在[0,p-1]内，重复上述
- * 如果k>p+1，说明要找的数在[p+1,end]内，重复上述
+ * 比pivot大的元素都在右边，比pivot小的元素都在左边，数组按照从小到大的顺序排列
+ * 如果在右边,那么依然寻找第K大的数
+ * 如果在左边,那么需要寻找第p+k-end-1大的数
+ * 如果end+1-k==p,说明pivot就是需要寻找的数
+ * 
+ * 还可以将数组按照从大到小的顺序排列
  **/
 public class FindNth {
 
-    //寻找数组中第k大的数
-    private static int solve(int[] num,int k){
-        return solve_findnth(num,0,num.length-1,k);
+    public static int findK(int[] nums,int k){
+        return findK(nums,0,nums.length-1,k);
     }
 
-    private static int solve_findnth(int[] num,int start,int end,int k){
-        int p = partition(num, start, end);
-        //说明已经找到所需要的数，直接返回
-        if(p+1==k){
-            return num[p];
-        }else if(k<p+1){
-            return solve_findnth(num,start,p-1,k);
+    public static int findK(int[] nums,int start,int end,int k){
+        int p=partition(nums,start,end);
+        //如果end+1-k==p,说明pivot就是要找的元素，直接返回
+        if(end+1-k==p){
+            return nums[p];
+        }else if(end+1-k<p){
+            //说明第K大的元素在pivot的左边，那么寻找的是新数组里第p+k-end-1大的元素
+            return findK(nums,start,p-1,p+k-end-1);
         }else{
-            return solve_findnth(num,p+1,end,k);
+            //说明第K大的元素在pivot的右边，那么依然是寻找新数组中的第K大的元素
+            return findK(nums,p+1,end,k);
         }
     }
 
-    private static int partition(int[] num,int start,int end){
+    public static int partition(int[] nums,int start,int end){
+        int pivot=nums[end];
         int i=start;
-        int pivot=num[end];
-        for (int j = start; j <end ; j++) {
-            //把数组分为两个部分，左边是大于pivot的，i指向的是大于pivot的最后的位置
-            //右边是小于pivot的，j指向的是第一个小于pivot的位置
-            if(num[j]>pivot){
-                int tmp=num[i];
-                num[i]=num[j];
-                num[j]=tmp;
-                i+=1;
+        for(int j=start;j<=end;j++){
+            if(nums[j]<pivot){
+                int tmp=nums[j];
+                nums[j]=nums[i];
+                nums[i]=tmp;
+                i++;
             }
         }
-        num[end]=num[i];
-        num[i]=pivot;
+        nums[end]=nums[i];
+        nums[i]=pivot;
         return i;
     }
 
     public static void main(String[] args) {
-        int[] nums={6,1,3,5,7,2,4,9,11,8};
-        int solve = FindNth.solve(nums, 10);
-        System.out.println(solve);
+        int[] nums={4,2,5,12,3};
+        System.out.println(findK(nums,3));
     }
 }
